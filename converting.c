fddead79980b6893f47619c8a394f20cf5b34224
alexchu-afk <alexchu414@gmail.com>
2022-08-03 03:51:43 -0400

    Tokenizing.c commented. Format standized. Version 2 pre-added in the module c file. Converting.c header added, full comment is to be done.

--- converting.c
+++ converting.c
@@ -1,3 +1,12 @@
+/*
+Author:         Alex Chu, Kenneth, Dan
+Email:          kchu30@myseneca.ca
+ID:             153954219
+Date written:   2022-08-02
+Course:         CPR 101 NHH
+Project:        Final Project
+Purpose:        This module is to parse a numeric string argument to an integer by calling the atoi() function. From the original user input it is to be in ASCII value because it is a string, then it is being converted to an actual number having the value as typed.
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 80
 #include "converting.h"
--- tokenizing.c
+++ tokenizing.c
@@ -1,64 +1,60 @@
-/*
-*****************************************************************************
-                          CPR101 Final Group Project
-Full Name  : Alex Chu
-Student ID#: 153954219
-Email      : kchu30@myseneca.ca
-Section    : NHH
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-*****************************************************************************
+/*/*
+Author:         Alex Chu
+Email:          kchu30@myseneca.ca
+ID:             153954219
+Date written:   2022-08-02
+Course:         CPR 101 NHH
+Project:        Final Project
+Purpose:        This module is to tokenize a string by using strtok() and split them into tokens by using delimiters. The codes written use a space " " as a delimiters to separate a string into words and output them one by one giving the order number respectively. By upgrading the codes, it could be working with data files and output the data which use delimiters. E.g. An input as "Hello World" would be split down to "Hello" and "World".
 */
 
+//Header included, Macro definitions
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 300
 #include "tokenizing.h"
 
-
+//Function starts here. No argument passed and return null
 void tokenizing(void) {
 
-//Version 1 of the codes
+//Version 1
     printf("*** Start of Tokenizing Strings Demo ***\n");
-    char words[BUFFER_SIZE];
-    char* nextWord = NULL;
-    int wordsCounter;
+    char    words[BUFFER_SIZE]; //stores the input string from user
+    char*   nextWord = NULL;    //this is where the token takes place, string is modified and broken into tokens
+    int     wordsCounter;       //this variable stores the numbers of tokens
     do {
-        printf("Type a few words seperated by space (q - to quit):\n");
+        printf("Type a few words seperated by space (q - to quit):\n"); 
         fgets(words, BUFFER_SIZE, stdin);
-        words[strlen(words) - 1] = '\0';
-        if (strcmp(words, "q") != 0) {
-            nextWord = strtok(words, " ");
+        words[strlen(words) - 1] = '\0';    //define the null terminator
+        if (strcmp(words, "q") != 0) {      //compare the input and only exit when receive "q"
+            nextWord = strtok(words, " ");  //string lib function split string by delimiters, returns a pointer to the first token in word
             wordsCounter = 1;
-            while (nextWord) {
+            while (nextWord) {              //loop continues until there null terminator found, hence it keeps spliting the string
                 printf("Word #%d is \'%s'\n", wordsCounter++, nextWord);
-                nextWord = strtok(NULL, " ");
-            }
-        }
-    } while (strcmp(words, "q") != 0);
+                nextWord = strtok(NULL, " "); // returns a pointer to the next token in the string
+            }       //end of while loop when reach null terminator
+        }       //end of if
+    } while (strcmp(words, "q") != 0);      //end while loop when 
     printf("*** End of Tokenizing Words Demo ***\n\n");
 
-/*Version 2 of the codes
+/*Version 2
     printf("*** Start of Tokenizing Phrases Demo ***\n");
     char phrases[BUFFER_SIZE];
     char* nextPhrase = NULL;
-    int wordsCounter;
+    int phrasesCounter;
     do {
-        printf("Type a few words seperated by space (q - to quit):\n");
-        fgets(words, BUFFER_SIZE, stdin);
-        words[strlen(words) - 1] = '\0';
-        if (strcmp(words, "q") != 0) {
-            nextWord = strtok(words, " ");
-            wordsCounter = 1;
-            while (nextWord) {
-                printf("Word #%d is \'%s'\n", wordsCounter++, nextWord);
-                nextWord = strtok(NULL, " ");
+        printf("Type a few phrases seperated by comma (q - to quit):\n");
+        fgets(phrases, BUFFER_SIZE, stdin);
+        phrases[strlen(phrases) - 1] = '\0';
+        if (strcmp(phrases, "q") != 0) {
+            nextPhrase = strtok(phrases, ",");
+            phrasesCounter = 1;
+            while (nextPhrase) {
+                printf("Phrase #%d is \'%s'\n", phrasesCounter++, nextPhrase);
+                nextPhrase = strtok(NULL, ",");
             }
         }
-    } while (strcmp(words, "q") != 0);
-    printf("*** End of Tokenizing Words Demo ***\n\n");
+    } while (strcmp(phrases, "q") != 0);
+    printf("*** End of Tokenizing Phrases Demo ***\n\n");
 */
 
 
