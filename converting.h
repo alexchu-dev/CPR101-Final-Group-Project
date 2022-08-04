366cb1c45f70c86e5c5e318f8ee9dd8dc507d004
alexchu-afk <alexchu414@gmail.com>
2022-08-04 01:23:04 -0400

    Modified V1 comments in  tokenizing.c, tokenizing.h Initialised V1 comments in converting.c, tokenizing.h

--- converting.h
+++ converting.h
@@ -1,7 +1,17 @@
+/*
+Author:         Alex Chu(V1), Kenneth Reforma(V2), Dan Zhang(V3)
+Email:          kchu30@myseneca.ca, kreforma@myseneca.ca, dzhang101@myseneca.ca
+ID:             153954219, 144680212, 182201210
+Date written:   2022-08-04(V1), 2022-08-dd(V2), 2022-08-dd(V3)
+Course:         CPR 101 NHH
+Project:        Final Project
+Purpose:        This module is to parse a numeric string argument to an integer by calling the atoi() function. From the original user input it is to be in ASCII value because it is a string, then it is being converted to an actual number having the value as typed.
+*/
+
 #ifndef _CONVERTING_H_
 #define _CONVERTING_H_
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h>	//include standard IO functions
+#include <string.h>	//include string lib functions
+#include <stdlib.h>	//include extended standard lib functions
 void converting(void);
 #endif // !_CONVERTING_H_
--- tokenizing.c
+++ tokenizing.c
@@ -1,8 +1,8 @@
-/*/*
+/*
 Author:         Alex Chu
 Email:          kchu30@myseneca.ca
 ID:             153954219
-Date written:   2022-08-02
+Date written:   2022-08-02(V1)
 Course:         CPR 101 NHH
 Project:        Final Project
 Purpose:        This module is to tokenize a string by using strtok() and split them into tokens by using delimiters. The codes written use a space " " as a delimiters to separate a string into words and output them one by one giving the order number respectively. By upgrading the codes, it could be working with data files and output the data which use delimiters. E.g. An input as "Hello World" would be split down to "Hello" and "World".
@@ -18,22 +18,24 @@ void tokenizing(void) {
 
 //Version 1
     printf("*** Start of Tokenizing Strings Demo ***\n");
-    char    words[BUFFER_SIZE]; //stores the input string from user
+    char    words[BUFFER_SIZE]; //this variable is to store the input string from user
     char*   nextWord = NULL;    //this is where the token takes place, string is modified and broken into tokens
     int     wordsCounter;       //this variable stores the numbers of tokens
+
+    //Start of do-while loop, exits when "q" is received
     do {
-        printf("Type a few words seperated by space (q - to quit):\n"); 
+        printf("Type a few words seperated by space (q - to quit):\n");
         fgets(words, BUFFER_SIZE, stdin);
-        words[strlen(words) - 1] = '\0';    //define the null terminator
-        if (strcmp(words, "q") != 0) {      //compare the input and only exit when receive "q"
+        words[strlen(words) - 1] = '\0';    //define the null terminator at the end of the string
+        if (strcmp(words, "q") != 0) {      //compare the input with the exiting string "q"
             nextWord = strtok(words, " ");  //string lib function split string by delimiters, returns a pointer to the first token in word
-            wordsCounter = 1;
+            wordsCounter = 1;               //initialise the wordsCounter so that it starts with 1 in the display
             while (nextWord) {              //loop continues until there null terminator found, hence it keeps spliting the string
                 printf("Word #%d is \'%s'\n", wordsCounter++, nextWord);
                 nextWord = strtok(NULL, " "); // returns a pointer to the next token in the string
             }       //end of while loop when reach null terminator
         }       //end of if
-    } while (strcmp(words, "q") != 0);      //end while loop when 
+    } while (strcmp(words, "q") != 0);      //end of loop
     printf("*** End of Tokenizing Words Demo ***\n\n");
 
 /*Version 2
